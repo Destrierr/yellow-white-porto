262e2f878bef35bf324cd80f5ee795a3371cae03
Destri R <93755456+Destrierr@users.noreply.github.com>
2025-05-28 08:39:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// function action1() {
+//     alert("Button 1 clicked!");
+// }
+
+// function action2() {
+//     alert("Button 2 clicked!");
+// }
+
+// function action3() {
+//     alert("Button 3 clicked!");
+// }
+
+// function action4() {
+//     alert("Button 4 clicked!");
+// }
